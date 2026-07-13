fd2655af1d0ed9a76478ea552e63d907619be462
robddaniels <93395645+robddaniels@users.noreply.github.com>
2026-07-12 23:34:38 -0400

    July 2026 Update in-the-news.php

--- in-the-news.php
+++ in-the-news.php
@@ -8,6 +8,24 @@
 <section class="content-block in-the-news-content">
 	<article>
     	<div class="in-the-news-content-inner">
+	    	<h4>In Haiti, a Note of Grace in Dark Times<br/>&#8211; The East Hampton Star</h4>
+    	    <a href="https://www.easthamptonstar.com/index.php/villages/202679/haiti-note-grace-dark-times" target="_blank">Continue to Article</a>
+        </div>
+    </article>
+	<article>
+    	<div class="in-the-news-content-inner">
+	    	<h4>Wings Over Haiti Continues Mission To Educate Children<br/>&#8211; The Sag Harbor Express</h4>
+    	    <a href="https://www.27east.com/sag-harbor-express/news/education/article_2ff40f12-b4f9-4775-9405-bb72f7f0567b.html" target="_blank">Continue to Article</a>
+        </div>
+    </article>
+	<article>
+    	<div class="in-the-news-content-inner">
+	    	<h4>Hamptons Artists Benefit for Haiti &#8211; July 18th<br/>&#8211; The Montauk Sun</h4>
+    	    <a href="https://www.montauksun.com/hamptons-artists-benefit-for-haiti-july-18th/" target="_blank">Continue to Article</a>
+        </div>
+    </article>
+	<article>
+    	<div class="in-the-news-content-inner">
 	    	<h4>Opinion | In Haiti, where is hope?<br/>&#8211; The Washington Post</h4>
     	    <a href="https://www.washingtonpost.com/opinions/2024/06/23/haiti-earthquake-hurricane-chaos-glynn-schools/" target="_blank">Continue to Article</a>
         </div>
@@ -32,38 +50,14 @@
     </article>
     <article>
     	<div class="in-the-news-content-inner">
-	    	<h4>Haiti’s Crisis Is Now Ted's, Too<br/>&#8211; Easthampton Star</h4>
-    	    <a href="https://www.easthamptonstar.com/News/2018530/Haitis-Crisis-Now-Teds-Too">Continue to Article</a>
-        </div>
-    </article>
-    <article>
-    	<div class="in-the-news-content-inner">
-	    	<h4>Art Auction To Benefit Wings Over Haiti Returns To Water Mill<br/>&#8211; 27East.com</h4>
-    	    <a href="http://www.27east.com/news/article.cfm/Water-Mill/558320/Art-Auction-To-Benefit-Wings-Over-Haiti-Returns-To-Water-Mill">Continue to Article</a>
-        </div>
-    </article>
-    <article>
-    	<div class="in-the-news-content-inner">
 	    	<h4>Wings Over Haiti Takes Flight<br/>&#8211; Huffpost.com</h4>
     	    <a href="https://www.huffpost.com/entry/haiti-recovery-efforts_b_1084871">Continue to Article</a>
         </div>
     </article>
     <article>
     	<div class="in-the-news-content-inner">
-	    	<h4>'Wings Over Haiti' Soars Again<br/>&#8211; 27East.com</h4>
-    	    <a href="http://www.27east.com/news/article.cfm/East-End/592994/Wings-Over-Haiti-Soars-Again">Continue to Article</a>
-        </div>
-    </article>
-    <article>
-    	<div class="in-the-news-content-inner">
 	    	<h4>Long Islander starts school to help children in Haiti<br/>&#8211; News 12 Long Island</h4>
     	    <a href="http://longisland.news12.com/story/34748992/long-islander-starts-school-to-help-children-in-haiti">Continue to Article</a>
-        </div>
-    </article>
-    <article>
-    	<div class="in-the-news-content-inner">
-	    	<h4>Hope Amid the Heartbreak in Haiti<br/>&#8211; Easthampton Star</h4>
-    	    <a href="https://www.easthamptonstar.com/News/2012119/Hope-Amid-Heartbreak-Haiti">Continue to Article</a>
         </div>
     </article>
 </section>
